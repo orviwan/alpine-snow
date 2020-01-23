065d859df73f858fa3d84997fdef28876c25992d
Jon Barlow <jon.barlow@fitbit.com>
2020-01-23 21:06:08 +0000

    Changed exercise name and type

--- app/config.js
+++ app/config.js
@@ -1,4 +1,5 @@
-export const exerciseName = "cycling"; //"Alpine Snow";
+export const exerciseName = "Alpine Snow";
+export const exerciseType = "cycling";
 
 export const exerciseOptions = {
   gps: true
